45f575a5819cefd632da9bdaca3b27618ac17999
Farhad <kadirovezfarhad@gmail.com>
2026-06-22 14:47:57 +0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,13 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    
     # Application ==============================
+    
     app_name: str = 'template-fastapi'
     frontend_url: str = 'portal.hmc.az'
     app_version: str = '1.0.0'
     debug: bool = True
     environment: str = 'production'
+    
     # Database =================================
+    
     database_engine: str = 'postgresql+asyncpg'
     database_ip: str = 'dpg-d8sdv6e7r5hc73fcg1o-a'
     database_port: str = '5432'
@@ -15,7 +19,9 @@ class Settings(BaseSettings):
     database_username: str = 'faroosha'
     database_password: str = 'jQmyUKKVpfppyc5xUGk7X6JfAm3Jhi5u'
     database_url_override: str = ''
+    
     # Security =================================
+    
     encryption_key: str = ''
     secret_key: str = 'blablablasecretkeytest'
     algorithm: str = 'HS256'
@@ -29,7 +35,9 @@ class Settings(BaseSettings):
     email_code_timeout: int = 10
     session_email_code_limit: int = 10
     session_expire_minute: int = 15
+    
     # Password policy ==========================
+    
     password_min_length: int = 10
     password_require_uppercase: bool = True
     password_require_lowercase: bool = True
@@ -37,7 +45,10 @@ class Settings(BaseSettings):
     password_require_symbol: bool = True
     password_min_life_hours: int = 24
     password_max_life_days: int = 90
+    
     # Mail server ==============================
+
+    resend_api_key: str = 're_B8ugUxA8_4RtbQgKvkScityAxs9zr2zkg'
     smtp_server: str = 'smtp.gmail.com'
     smtp_port: int = 465
     smtp_user: str = 'aligavali228@gmail.com'

67b72ebb0f678ae08c5593af45db820182d072ea
Farhad <kadirovezfarhad@gmail.com>
2026-06-22 17:45:59 +0400

    Update email.py

--- backend/utils/email.py
+++ backend/utils/email.py
@@ -39,39 +39,6 @@ async def send_email(
         if response.status_code != 201:
             raise Exception(f"Failed to send email: {response.text}")
 
-# async def send_email(
-#         receiver_email: str,
-#         subject: str,
-#         body: str
-# ) -> None:
-#     """Send email via SMTP. Native async using aiosmtplib."""
-#     try:
-#             resend.Emails.send({
-#                     "from": "SuperChat <onboarding@resend.dev>",
-#         "to": receiver_email,
-#         "subject": subject,
-#         "html": body
-#             })
-        # msg = MIMEMultipart()
-        # msg["Reply-To"] = settings.smtp_sender_email
-        # msg["From"] = formataddr((settings.smtp_sender_name, settings.smtp_sender_email))
-        # msg["To"] = receiver_email
-        # msg["Subject"] = subject
-        # msg.attach(MIMEText(body, "html"))
-
-        # ssl_context = ssl.create_default_context()
-        # ssl_context.check_hostname = False
-        # ssl_context.verify_mode = ssl.CERT_NONE
-
-        # await aiosmtplib.send(
-        #     msg,
-        #     hostname=settings.smtp_server,
-        #     port=settings.smtp_port,
-        #     username=settings.smtp_user,
-        #     password=settings.smtp_password,
-        #     start_tls=True,
-        #     tls_context=ssl_context,
-        # )
 
 
     except Exception as e:

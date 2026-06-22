428d31b98ca8f842f45bf9d7a7a1180f17f694f0
Farhad <kadirovezfarhad@gmail.com>
2026-06-22 13:51:22 +0400

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,15 +1,12 @@
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
     # Application ==============================
     app_name: str = 'template-fastapi'
     frontend_url: str = 'portal.hmc.az'
     app_version: str = '1.0.0'
     debug: bool = True
     environment: str = 'production'
-
     # Database =================================
     database_engine: str = 'postgresql+asyncpg'
     database_ip: str = 'dpg-d8sdv6e7r5hc73fcg1o-a'
@@ -17,38 +14,33 @@ class Settings(BaseSettings):
     database_name: str = 'chatdatabase_a7jq'
     database_username: str = 'faroosha'
     database_password: str = 'jQmyUKKVpfppyc5xUGk7X6JfAm3Jhi5u'
-
+    database_url_override: str = ''
     # Security =================================
-    encryption_key : str = ''
+    encryption_key: str = ''
     secret_key: str = 'blablablasecretkeytest'
-    algorithm : str = 'HS256'
-    access_token_expire_minutes : int = 15
-    refresh_token_expire_days : int = 30
-    ip_check_enabled : bool = True
-    max_attempt_per_ip : int = 10
-    rate_limit_minutes : int = 5
-
-    user_email_code_limit : int = 10
-    user_bad_password_limit : int = 5
-
-    email_code_timeout : int = 10
-
-    session_email_code_limit : int = 10
-    session_expire_minute : int = 15
-
+    algorithm: str = 'HS256'
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 30
+    ip_check_enabled: bool = True
+    max_attempt_per_ip: int = 10
+    rate_limit_minutes: int = 5
+    user_email_code_limit: int = 10
+    user_bad_password_limit: int = 5
+    email_code_timeout: int = 10
+    session_email_code_limit: int = 10
+    session_expire_minute: int = 15
     # Password policy ==========================
-    password_min_length : int = 10
-    password_require_uppercase : bool = True
-    password_require_lowercase : bool = True
-    password_require_digit : bool = True
-    password_require_symbol : bool = True
-    password_min_life_hours : int = 24
-    password_max_life_days : int = 90
-
+    password_min_length: int = 10
+    password_require_uppercase: bool = True
+    password_require_lowercase: bool = True
+    password_require_digit: bool = True
+    password_require_symbol: bool = True
+    password_min_life_hours: int = 24
+    password_max_life_days: int = 90
     # Mail server ==============================
     smtp_server: str = 'smtp.gmail.com'
     smtp_port: int = 587
-    smtp_user:str = 'aligavali228@gmail.com'
+    smtp_user: str = 'aligavali228@gmail.com'
     smtp_password: str = 'wkevqeljeoyzixdw'
     smtp_sender_email: str = 'aligavali228@gmail.com'
     smtp_sender_name: str = 'SuperChat'
@@ -66,6 +58,8 @@ class Settings(BaseSettings):
 
     @property
     def database_url(self) -> str:
+        if self.database_url_override:
+            return self.database_url_override
         return (
             f'{self.database_engine}://'
             f'{self.database_username}:{self.database_password}@'
@@ -73,6 +67,4 @@ class Settings(BaseSettings):
             f'{self.database_name}'
         )
 
-
-# Singleton instance
 settings = Settings()
